cdeb366dcba375c0f43b6b9af3e0746542ee6874
c3606024 <s.arif2453@student.leedsbeckett.ac.uk>
2024-11-11 11:16:39 +0000

    amended the index route to point to the homepage

--- resources/views/components/background-layout.blade.php
+++ resources/views/components/background-layout.blade.php
@@ -7,8 +7,8 @@
     <body>
     <header>
     @auth
-    <div class="sm:flex sm:items-center sm:ml-6">
-        <x-dropdown align="right" width="48">
+    <div class="sm:flex sm:items-center sm:ml-6 mx-48 px-48">
+        <x-dropdown class="mx-48" align="right" width="48">
             <x-slot name="trigger">
                 <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                     <div>{{ Auth::user()->name }}</div>
@@ -29,6 +29,13 @@
                 </form>
             </x-slot>
         </x-dropdown>
+    </div>
+    <div>
+        @if(Auth::user()->is_admin==1)
+            Administrator
+        @else
+            Customer
+        @endif
     </div>
     @else
     <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', [ProductController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
