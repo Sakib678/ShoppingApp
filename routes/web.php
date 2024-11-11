f321d0070d0444d4749eef19b325082ff77ca85a
c3606024 <s.arif2453@student.leedsbeckett.ac.uk>
2024-11-11 12:04:30 +0000

    added the product as a constructor

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Http\Request; 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
