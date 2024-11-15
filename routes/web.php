5ade38baa6566149c8b320ec0b187ebcad355d9b
c3606024 <s.arif2453@student.leedsbeckett.ac.uk>
2024-11-15 15:58:10 +0000

    added 2 routes to index and show

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 /*Route::get('/', function () {
     return view('welcome');
 });*/
-Route::get('/', [ProductController::class, 'index'])->name('home');
+Route::get('/product', [ProductController::class, 'index']);
 Route::get('/product/{id}', [ProductController::class, 'show']);
 
 Route::get('/dashboard', function () {
