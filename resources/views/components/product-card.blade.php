6bf7c224aa0c25d3f0b5b51ad5837617cdeac59f
c3606024 <s.arif2453@student.leedsbeckett.ac.uk>
2024-11-22 14:01:07 +0000

    added a javascript event listener for the product buttons

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -16,10 +16,10 @@
    <div class="rounded-sm bg-yellow-300 hover:bg-blue-700 p-2 m-2 w-24">  
     <a href="/product/{{$product->id}}" class="text-gray-700">Select</a>
    </div>
-   <button value="" 
+   <button value="{{$product->id}}" 
       class="bg-yellow-300 hover:bg-blue-700 text-gray-700 p-2 m-2 w-24 rounded-sm select-product">
       SelectJS
-   </button>
+    </button>
 </div>
 </div>
 
