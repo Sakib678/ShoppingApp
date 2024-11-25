ca3ad9564c1163906a6f64ef8f4636aee5e9807f
c3606024 <s.arif2453@student.leedsbeckett.ac.uk>
2024-11-25 11:00:12 +0000

    added store and update routes

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,12 @@ use Illuminate\Support\Facades\Route;
 });*/
 Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
 Route::get('/product/{id}/edit', [ProductController::class, 'edit'])->name('product.edit');
-Route::get('/', [ProductController::class, 'index'])->name('home');
 Route::get('/product/{id}', [ProductController::class, 'show']);
+Route::get('/', [ProductController::class, 'index'])->name('home');
 
+
+Route::post('/product', [ProductController::class, 'store'])->name('product.store');
+Route::put('/product/{id}', [ProductController::class, 'update'])->name('product.update');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
