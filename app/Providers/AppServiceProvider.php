d1aaa9ff0967448f634bcaab7853f03b2c4e7856
c3606024 <s.arif2453@student.leedsbeckett.ac.uk>
2024-12-02 11:07:24 +0000

    added gate to appserviceprovider file

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,9 +21,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-       // If using Che then uncomment to force https
+        
+        Gate::define('can-edit-product', function (User $user) {
+            return ($user->is_admin == 1); //expression equates to true/false
+        
+        }
+        // If using Che then uncomment to force https
        // otherwise files like css on server will not load
        
           \URL::forceScheme('https');
-    }
+    
 }
