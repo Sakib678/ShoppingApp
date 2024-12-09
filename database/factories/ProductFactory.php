d393032a5c25cae7b0aef2c48e035d7535aa9e42
c3606024 <s.arif2453@student.leedsbeckett.ac.uk>
2024-12-09 11:44:29 +0000

    added the faker to create products

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -1,6 +1,7 @@
 <?php
-
 namespace Database\Factories;
+use Faker\Factory as Faker;
+use App\Models\Product;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -11,13 +12,20 @@ class ProductFactory extends Factory
 {
     /**
      * Define the model's default state.
+     * 
      *
      * @return array<string, mixed>
      */
     public function definition(): array
     {
+        $faker=Faker::create();
+        
         return [
-            //
+            
+            'name' => $this->faker->name(),
+            'title' => $this->faker->sentence(3),
+            'price' => $this->faker->numberBetween(1,999),
+            'product_type_id'=> $this->faker->numberBetween(1,3),
         ];
     }
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Product;
+use Database\Factories\ProductFactory;
 
 class ProductSeeder extends Seeder
 {
@@ -13,20 +14,11 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        $products = [
-            ['name' => 'The Jam', 'title' => 'Modern World','price' => 399, 'producttype' => 2, 'product_type_id' => 1],
-            ['name' => 'Amy Winehouse', 'title' => 'Back to Black','price' => 299, 'producttype' => 2, 'product_type_id' => 2],
-        ];
+        Product::factory()->count(50)->create();
 
-        foreach($products as $product) {
-            Product::create([
-                'name' => $product['name'],
-                'title' => $product['title'],
-                'price' => $product['price'],
-                'producttype' => $product['producttype'],
-                'product_type_id'=> $product['product_type_id'],
+        
 
-            ]);
+            
         }
     }
-}
+
