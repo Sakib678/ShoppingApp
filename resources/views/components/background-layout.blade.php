89f2e08ee8624ff177b29bf3e17422f5141f77b1
c3606024 <s.arif2453@student.leedsbeckett.ac.uk>
2024-12-12 17:16:19 +0000

    added the menu bar

--- resources/views/components/background-layout.blade.php
+++ resources/views/components/background-layout.blade.php
@@ -1,62 +1,19 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <title>Laravel</title>
-         <script src="{{ asset('js/my.js') }}" ></script>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-     </head>
-    <body>
-     <header>
-        @auth
-        <div class="sm:flex sm:items-center sm:ml-6 mx-48 px-48">
-          <x-dropdown class="mx-48" align="right" width="48">
-              <x-slot name="trigger">
-                  <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                      <div>{{ Auth::user()->name }}</div>
-                      <div class="ml-1">
-                         <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                              <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                          </svg>
-                      </div>
-                    </button>
-             </x-slot>
-            <x-slot name="content">
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <x-dropdown-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-dropdown-link>
-                </form>
-            </x-slot>
-            </x-dropdown>
-     </div>
-    <div>
-        @if(Auth::user()->is_admin==1)
-            Administrator
-            
-        @else
-            Customer
-        @endif
-    </div>
-    @else
-        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
-        @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-         @endif
-    @endif
-    @can('create', App\Models\Product::class)
-    <a href="{{ route('product.create') }}" class="text-sm text-gray-700 underline">Add Product</a>
-    @endcan
-    
-</header>
+<head>
+    <title>Laravel</title>
+    <script src="{{ asset('js/my.js') }}"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    @include('components.menu-bar') 
 
- 
-        <section>
-            <p>MAIN CONTENT</p>
-            {{$slot}}
-        </section>
-        <footer>
-            <p>Made by Sakib Arif c3606024 Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</p>
-        </footer>
-    </body>
+    <section>
+        <p>MAIN CONTENT</p>
+        {{ $slot }}
+    </section>
+    <footer>
+        <p>Made by Sakib Arif c3606024 Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</p>
+    </footer>
+</body>
 </html>
